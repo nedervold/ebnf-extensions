8389ca8d187530633f32f733c403f4b863cdd026
Eric Nedervold <nedervoldsoftware@gmail.com>
2019-10-02 19:42:32 -0700

    Added Generic.

--- src/Language/Ebnf/Extensions/Syntax.hs
+++ src/Language/Ebnf/Extensions/Syntax.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -6,12 +7,13 @@
 
 module Language.Ebnf.Extensions.Syntax where
 
-import Control.Lens.TH(makeClassy)
+import Control.Lens.TH (makeClassy)
 import Data.Bifoldable
 import Data.Bifunctor
 import Data.Bitraversable
 import Data.List.NonEmpty
 import Data.Ord (comparing)
+import GHC.Generics (Generic)
 
 type Opt = Maybe
 
@@ -24,8 +26,8 @@ type Rep1 = Data.List.NonEmpty.NonEmpty
 ------------------------------------------------------------
 data Repsep0 s b
   = Repsep0Nothing
-  | Repsep0Just { _repsep0Contents :: Repsep1 s b }
-  deriving (Eq, Functor, Ord, Show)
+  | Repsep0Just { _repsep0Contents :: Repsep1 s b}
+  deriving (Eq, Functor, Generic, Ord, Show)
 
 instance Foldable (Repsep0 s) where
   foldMap = bifoldMap (const mempty)
@@ -51,7 +53,7 @@ data Repsep1 s b
   | Repsep1Cons { _repsep1Body :: b
                ,  _repsep1Separator :: s
                ,  _repsep1Tail :: Repsep1 s b}
-  deriving (Eq, Functor, Show)
+  deriving (Eq, Functor, Generic, Show)
 
 instance (Ord b, Ord s) =>
          Ord (Repsep1 s b) where
@@ -74,7 +76,8 @@ instance Bifunctor Repsep1 where
 
 instance Bifoldable Repsep1 where
   bifoldMap _f g (Repsep1Singleton b) = g b
-  bifoldMap f g (Repsep1Cons b s rs1) = mconcat [g b, f s, bifoldMap f g rs1]
+  bifoldMap f g (Repsep1Cons b s rs1) =
+    mconcat [g b, f s, bifoldMap f g rs1]
 
 instance Bitraversable Repsep1 where
   bitraverse _f g (Repsep1Singleton b) = Repsep1Singleton <$> g b
@@ -82,6 +85,6 @@ instance Bitraversable Repsep1 where
     Repsep1Cons <$> g b <*> f s <*> bitraverse f g rs1
 
 ------------------------------------------------------------
-
 makeClassy ''Repsep0
+
 makeClassy ''Repsep1
