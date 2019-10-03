19ff0d5ce810776cacdb40afa431d183504d800f
Eric Nedervold <nedervoldsoftware@gmail.com>
2019-10-02 19:30:39 -0700

    Added tests for the parsers.

--- test/Language/Ebnf/Extensions/Spec.hs
+++ test/Language/Ebnf/Extensions/Spec.hs
@@ -10,22 +10,28 @@ module Language.Ebnf.Extensions.Spec
 -- spec_: Hspec specifications.
 -- test_: Tasty TestTrees.
 -}
+import Data.Void (Void)
 import Hedgehog (Gen, Range)
 import Hedgehog.Classes
-       (foldableLaws, functorLaws, traversableLaws, bifoldableLaws,
-        bifoldableFunctorLaws, bifunctorLaws, bitraversableLaws, ordLaws,
-        lawsCheckMany)
+       (bifoldableFunctorLaws, bifoldableLaws, bifunctorLaws,
+        bitraversableLaws, foldableLaws, functorLaws, lawsCheckMany,
+        ordLaws, traversableLaws)
 import Hedgehog.Gen (alpha, alphaNum, list)
 import Hedgehog.Range (linear)
 import Language.Ebnf.Extensions.Generators (genRepsep0, genRepsep1)
-import Language.Ebnf.Extensions.Syntax ()
+import Language.Ebnf.Extensions.Parsers
+       (parseRepsep0, parseRepsep1)
+import Language.Ebnf.Extensions.Syntax (Repsep0(..), Repsep1(..))
+import Test.Hspec.Megaparsec (shouldFailOn, shouldParse)
 import Test.Tasty.Hspec (Spec, describe, it, shouldBe)
+import Text.Megaparsec (Parsec, parse)
+import Text.Megaparsec.Char (char)
 
 spec_extensions :: Spec
 spec_extensions = do
   let rng = linear 1 100
-  describe "Language.Ebnf.Extensions" $
-    it "obeys its laws" $ do
+  describe "Language.Ebnf.Extensions" $ do
+    it "obeys typeclass laws" $ do
       passed <-
         lawsCheckMany
           [ ( "Repsep0"
@@ -49,6 +55,43 @@ spec_extensions = do
               ])
           ]
       passed `shouldBe` True
+    describe "parsing Repsep0" $ do
+      it "parses nothing" $
+        parse parseRs0 "<test>" "" `shouldParse` Repsep0Nothing
+      it "parses \"x\"" $
+        parse parseRs0 "<test>" "x" `shouldParse`
+        Repsep0Just (Repsep1Singleton 'x')
+      it "parses \"x,x\"" $
+        parse parseRs0 "<test>" "x,x" `shouldParse`
+        Repsep0Just (Repsep1Cons 'x' ',' $ Repsep1Singleton 'x')
+      it "doesn't parse \"x,y\"" $
+        parse parseRs0 "<test>" `shouldFailOn` "x,y"
+      it "doesn't parse \"x,x,\"" $
+        parse parseRs0 "<test>" `shouldFailOn` "x,x,"
+      it "parses \"x,x,x\"" $
+        parse parseRs0 "<test>" "x,x,x" `shouldParse`
+        Repsep0Just
+          (Repsep1Cons 'x' ',' $ Repsep1Cons 'x' ',' $ Repsep1Singleton 'x')
+    describe "parsing Repsep1" $ do
+      it "parses nothing" $ parse parseRs1 "<test>" `shouldFailOn` ""
+      it "parses \"x\"" $
+        parse parseRs1 "<test>" "x" `shouldParse` Repsep1Singleton 'x'
+      it "parses \"x,x\"" $
+        parse parseRs1 "<test>" "x,x" `shouldParse`
+        Repsep1Cons 'x' ',' (Repsep1Singleton 'x')
+      it "doesn't parse \"x,y\"" $
+        parse parseRs1 "<test>" `shouldFailOn` "x,y"
+      it "doesn't parse \"x,x,\"" $
+        parse parseRs1 "<test>" `shouldFailOn` "x,x,"
+      it "parses \"x,x,x\"" $
+        parse parseRs1 "<test>" "x,x,x" `shouldParse`
+        Repsep1Cons 'x' ',' (Repsep1Cons 'x' ',' $ Repsep1Singleton 'x')
+
+parseRs0 :: Parsec Void String (Repsep0 Char Char)
+parseRs0 = parseRepsep0 (char ',') (char 'x')
+
+parseRs1 :: Parsec Void String (Repsep1 Char Char)
+parseRs1 = parseRepsep1 (char ',') (char 'x')
 
 genIdent :: Range Int -> Gen String
 genIdent rng = do
